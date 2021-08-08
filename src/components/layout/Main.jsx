621f9ebdac55266a634739b710e3c0d5a9f21b20
Deni Naurkhanov <naurkhanovdeni87@gmail.com>
2021-08-08 14:53:20 +0300

    gh pages add

--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -7,7 +7,7 @@ function Main() {
   const [loading, setLoading] = useState(true);
   const API_KEY = process.env.REACT_APP_API_KEY;
   useEffect(() => {
-    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
+    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}=matrix`)
       .then((response) => response.json())
       .then((data) => setMovies(data.Search, setLoading(false)));
   }, []);
@@ -17,7 +17,7 @@ function Main() {
   const SearchMovies = async (str, categories) => {
     setLoading(true);
     await fetch(
-      `https://www.omdbapi.com/?apikey=78584b3c&s=${str}${
+      `https://www.omdbapi.com/?apikey=${API_KEY}=${str}${
         categories !== 'all' ? `&type=${categories}` : ''
       }`,
     )
